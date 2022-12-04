836f4f66f94724274940a657586287cc8dfecd38
akashprasadram <akashprasadram99@gmail.com>
2022-12-04 23:15:33 +1100

    Junit Test

---
+++ user/src/main/java/com/akash/spring_junit_test_app/user/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.akash.spring_junit_test_app.user.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.akash.spring_junit_test_app.user.enities.User;
+import com.akash.spring_junit_test_app.user.services.UserServices;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserServices service;
+
+    @RequestMapping(value ="/",method = RequestMethod.GET)
+    public List<User> getAllUsers()
+    {
+        return service.getUsers();
+    }
+
+    @RequestMapping(value="/{id}",method = RequestMethod.GET)
+    public User getUserById(@PathVariable("id") int id)
+    {
+        return service.getUserById(id);
+    }
+
+    @RequestMapping(value ="/",method = RequestMethod.POST)
+    public User addUser(@RequestBody User user)
+    {
+        return service.addUser(user);
+    }
+
+    @RequestMapping(value="/",method = RequestMethod.PUT)
+    public User updateUser(@RequestBody User user)
+    {
+        return service.updateUser(user);
+    }
+
+    @RequestMapping(value="/",method = RequestMethod.DELETE)
+    public void deleteUser(@RequestBody User user)
+    {
+        service.deleteUser(user);
+    }
+}
